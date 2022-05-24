1861df0eb61e4c679f852bcd29da9deaa0a73434
Willian Roberg de Melo <roberg.willian@gmail.com>
2022-05-23 22:09:01 -0300

    feat: post route and get statement

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = 3333;
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.get('/statement', (req, res) => {
+    const {cpf} = req.headers
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer) {
+        return res.status(404).json({ error: "Customer not found" });
+    }
+
+    return res.json(customer.statement)
+})
+app.listen(PORT);
+console.log("Server listening on port " + PORT);
