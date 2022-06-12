ea87fd5dd6b17b4f29f0c90ada7d4d46216a63e8
Willian Roberg de Melo <roberg.willian@gmail.com>
2022-06-12 19:57:54 -0300

    adding middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { request } = require("http");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
@@ -7,6 +8,20 @@ const PORT = 3333;
 app.use(express.json());
 
 const customers = [];
+
+function verifyIfExisteAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(404).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
 
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
@@ -29,16 +44,12 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get('/statement', (req, res) => {
-    const {cpf} = req.headers
+// app.use(verifyIfExisteAccountCPF)
 
-    const customer = customers.find(customer => customer.cpf === cpf)
+app.get("/statement", verifyIfExisteAccountCPF, (req, res) => {
+  const { customer } = request;
+  return res.json(customer.statement);
+});
 
-    if(!customer) {
-        return res.status(404).json({ error: "Customer not found" });
-    }
-
-    return res.json(customer.statement)
-})
 app.listen(PORT);
 console.log("Server listening on port " + PORT);
